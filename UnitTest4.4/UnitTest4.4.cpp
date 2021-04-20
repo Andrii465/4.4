8cff94432d5a49a800dab6315587ce7480473057
Андрій Дудич <dudych92y@gmail.com>
2021-04-20 23:19:43 +0300

    first

---
+++ UnitTest4.4/UnitTest4.4.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest44
+{
+	TEST_CLASS(UnitTest44)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
